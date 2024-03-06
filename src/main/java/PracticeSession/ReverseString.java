30c906fd773a1e6da41e786719300a759f13b547
sharanbsaravanan <sharan1beta@gmail.com>
2024-03-06 12:28:14 +0530

    practice questions all are done

---
+++ src/main/java/PracticeSession/ReverseString.java
@@ -0,0 +1,17 @@
+package PracticeSession;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String  name= "Saravana";
+        String reverse = "";
+
+        for(int i = name.length()-1; i>=0; i--) {
+            reverse = reverse + name.charAt(i);
+        }
+
+            System.out.println("reverse name is :  " + reverse);
+
+
+    }
+
+}
